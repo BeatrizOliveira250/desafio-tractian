d5079602849c6b60d175e74d4c705b2ad0500f27
BeatrizOliveira250 <65451957+BeatrizOliveira250@users.noreply.github.com>
2020-09-06 17:43:55 -0300

    Update HashRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 
 
@@ -8,7 +8,7 @@ import Home from '../pages/Home';
 
 function Routes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/ativos" exact component={Ativos} />
@@ -22,7 +22,7 @@ function Routes() {
       )}
       />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
