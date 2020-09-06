9907b0348c9ef56b21d9ba43b2884ddc849a8a14
BeatrizOliveira250 <65451957+BeatrizOliveira250@users.noreply.github.com>
2020-09-06 16:33:17 -0300

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,17 +11,7 @@ function Routes() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/ativos" exact component={Ativos} />
-        <Route component={() => (
-          <div>
-            <h1>Página 404</h1>
-            <span>
-              Acho que você se perdeu no caminho.
-              <br />
-              Clique no título acima e volte para a página inicial.
-            </span>
-          </div>
-      )}
-      />
+
       </Switch>
     </BrowserRouter>
   );
