b678fd7f06baabd91e4bcf67bb61b7c2ef052f9f
BeatrizOliveira250 <65451957+BeatrizOliveira250@users.noreply.github.com>
2020-09-06 16:02:14 -0300

    Updates

---
+++ precache-manifest.c8c7ba4cdf840f89466274d20a2b84cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b7299970120e13065fd9686082b7d9",
+    "url": "/desafio-tractian/index.html"
+  },
+  {
+    "revision": "8fc3d946f40da6a196d7",
+    "url": "/desafio-tractian/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "3e0199f2b7ab074dd2ee",
+    "url": "/desafio-tractian/static/css/main.4f44eb31.chunk.css"
+  },
+  {
+    "revision": "8fc3d946f40da6a196d7",
+    "url": "/desafio-tractian/static/js/2.363112ae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/desafio-tractian/static/js/2.363112ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0199f2b7ab074dd2ee",
+    "url": "/desafio-tractian/static/js/main.b64070aa.chunk.js"
+  },
+  {
+    "revision": "949be21e4caae0231f1b",
+    "url": "/desafio-tractian/static/js/runtime-main.be0e4484.js"
+  }
+]);
