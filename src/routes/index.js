ded923806fe85c1b99e43c716afb40b47575365f
BeatrizOliveira250 <65451957+BeatrizOliveira250@users.noreply.github.com>
2020-09-06 17:24:51 -0300

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { HashRouter } from 'react-router-dom';
+
 
 import Ativos from '../pages/Ativos';
 import Home from '../pages/Home';
 
 function Routes() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/ativos" exact component={Ativos} />
         <Route component={() => (
           <div>
             <h1>Página 404</h1>
+            <span>
+              Acho que você se perdeu no caminho.
+            </span>
           </div>
       )}
       />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
